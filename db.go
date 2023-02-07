b6102222ecdaa6c79c289504a83e09b2361e3461
burgereza <srezaghamghaam@gmail.com>
2023-02-07 13:34:42 +0330

    Update db.go

--- db.go
+++ db.go
@@ -8,6 +8,7 @@ import (
 var db *sql.DB
 var err error
 
+// create new user in database
 func insertUser(username string, password string, phonenumber string) {
 
 	_, err = db.Exec("INSERT INTO users(username, password ,email ,phonenumber) VALUES(?,?,?,?)", username, password, phonenumber)
@@ -18,6 +19,7 @@ func insertUser(username string, password string, phonenumber string) {
 
 }
 
+// see if password is correct
 func checkUserInfo(username string, password string) bool {
 
 	var db_pass string
@@ -58,11 +60,42 @@ func getCities() []string {
 	return s
 }
 
-func connDB(host, user, password, dbname string, port int) {
+// for unique username
+func checkUsernameAvailability(username string) bool {
+
+	err := db.QueryRow("SELECT username FROM users WHERE username=?", username)
+
+	if err != nil {
+		return true
+	}
+
+	return false
+
+}
+
+// for unique email
+func checkEmailAvailability(username string) bool {
+
+	err := db.QueryRow("SELECT email FROM users WHERE username=?", username)
+
+	if err != nil {
+		return true
+	}
+
+	return false
+
+}
+
+func connDB(host, user, password, dbname string, port int) (db *sql.DB) {
+
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
+
 	if err != nil {
 		panic(err)
 	}
+
 	defer db.Close()
+
+	return db
 }
